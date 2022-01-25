5a4fb514455df62514b90504a5ff277931b97f3f
Ivan Gechev <ig@stptrans.com>
2022-01-25 17:45:01 +0200

    Additional problem solution added

--- 05.TopIntegers/Program.cs
+++ 05.TopIntegers/Program.cs
@@ -7,8 +7,22 @@ namespace _05.TopIntegers
     {
         static void Main()
         {
-            string[] inputArray = Console.ReadLine().Split().ToArray();
-            int[] intArray = new int[inputArray.Length];
+            int[] intArray = Console.ReadLine()
+                                    .Split()
+                                    .Select(int.Parse)
+                                    .ToArray();
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (i == intArray.Length - 1)
+                {
+                    Console.Write($"{intArray[i]} ");
+                }
+                else if (intArray[i] > intArray[i + 1])
+                {
+                    Console.Write($"{intArray[i]} ");
+                }
+            }
         }
     }
 }
