b38fdd93685ccee7fd11ed21a330001f6939cb03
Ivan Gechev <ivan_banov@abv.bg>
2022-02-11 12:54:41 +0200

    Added refs

---
+++ 05.BombNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.BombNumbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string[] args = Console.ReadLine().Split().ToArray();
+
+            int bomb = int.Parse(args[0]);
+            int power = int.Parse(args[1]);
+
+            int bombPosition = -1;
+
+            while (numbers.Contains(bomb))
+            {
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    if (numbers[i] == bomb)
+                    {
+                        bombPosition = i;
+                    }
+                }
+
+                int startIndex = bombPosition - power < 0 ? 0 : bombPosition - power;
+                int endIndex = bombPosition + power > numbers.Count - 1 ? numbers.Count - 1 : bombPosition + power;
+                int elementsToRemove = Math.Abs(endIndex - startIndex);
+                numbers.RemoveRange(startIndex, elementsToRemove + 1);
+            }
+
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
---
+++ 06.CardsGame/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.CardsGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> playerOne = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> playerTwo = Console.ReadLine().Split().Select(int.Parse).ToList();
+            
+            for (int i = 0; i < playerOne.Count; i++)
+            {
+                if(playerOne.Count == 0 || playerTwo.Count == 0)
+                {
+                    break;
+                }
+
+                if(playerOne[i] == playerTwo[i])
+                {
+                    playerOne.RemoveAt(i);
+                    playerTwo.RemoveAt(i);
+                }
+                else if (playerOne[i] > playerTwo[i])
+                {
+                    playerOne.Add(playerOne[i]);
+                    playerOne.Add(playerTwo[i]);
+                    playerOne.RemoveAt(i);
+                    playerTwo.RemoveAt(i);
+                }
+                else if (playerOne[i] < playerTwo[i])
+                {
+                    playerTwo.Add(playerTwo[i]);
+                    playerTwo.Add(playerOne[i]);
+                    playerOne.RemoveAt(i);
+                    playerTwo.RemoveAt(i);
+                }
+            }
+
+            if(playerOne.Count > 0)
+            {
+                Console.WriteLine($"First player wins!Sum: {playerOne.Sum()}");
+            }
+            else
+            {
+                Console.WriteLine($"Second player wins!Sum: {playerTwo.Sum()}");
+            }
+        }
+    }
+}
